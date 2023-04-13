06e8822749a0c4a77bfb835b38e376955c6053de
Greg <gjs1985@gmail.com>
2023-04-12 22:03:16 -0500

    latest changes

--- Mod3/CardList2/screen/HomeScreen.js
+++ Mod3/CardList2/screen/HomeScreen.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native-safe-area-context';
+import { cards } from "../data/cards";
+import CardList from '../components/CardList'
 
 
-const HomeScreen = ({navigation})=>{
+const HomeScreen = ()=>{
     return (
       <View style={styles.container}>
         <Text>Home</Text>
-        <SafeAreaView> <CardList items={cards}/> </SafeAreaView>
+    
       </View>
     );
 }
